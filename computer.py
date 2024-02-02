3acad1d1828940e97ef06571fa08cbd60815f58c
Rachel <rkunkel@smith.edu>
2024-02-02 11:15:38 -0500

    office hours

--- computer.py
+++ computer.py
@@ -3,27 +3,6 @@ from typing import Dict, Optional
 class Computer:
 
     # What attributes will it need?
-    description:str
-    operating_system:str
-    memory:int
-    price:int
-
-    def refurbish(self, inventory, item_id: int, new_os: Optional[str] = None):
-        if item_id in self.inventory:
-            computer = self.inventory[item_id] # locate the computer
-            if int(computer["year_made"]) < 2000:
-                computer["price"] = 0 # too old to sell, donation only
-            elif int(computer["year_made"]) < 2012:
-                computer["price"] = 250 # heavily-discounted price on machines 10+ years old
-            elif int(computer["year_made"]) < 2018:
-                computer["price"] = 550 # discounted price on machines 4-to-10 year old machines
-            else:
-                computer["price"] = 1000 # recent stuff
-
-            if new_os is not None:
-                computer["operating_system"] = new_os # update details after installing new OS
-        else:
-            print("Item", item_id, "not found. Please select another item to refurbish.")
 
     # How will you set up your constructor?
     # - Want to retain a description for each computer 
@@ -32,8 +11,14 @@ class Computer:
     # - Keep track of the price and update it when the computer is refurbished 
 
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__(self, inventory):
-        self.inventory = inventory
+    def __init__(self, description: str,
+                 processor_type: str,
+                 hard_drive_capacity:str,
+                 memory: int,
+                 operating_system:str,
+                 year_made:int,
+                 price:int):
+        pass
          # You'll remove this when you fill out your constructor
 
     # What methods will you need?
--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -4,43 +4,6 @@ class ResaleShop:
 
     # What attributes will it need?
     inventory = [] # computer objects will go in here
-
-    def __init__(self, inventory):
-        self.inventory = inventory 
-    
-    def inventory(self, inventory):
-        self.inventory : Dict[int, Dict] = {}
-        itemID = 0 # We'll increment this every time we add a new item 
-           # so that we always have a new value for the itemID
-        
-    def buy(self, computer: Dict):
-        global itemID
-        itemID += 1 # increment itemID
-        self.inventory[itemID] = computer
-        return itemID
-    
-    def update_price(self, inventory, item_id: int, new_price: int):
-        if item_id in self.inventory:
-            self.inventory[item_id]["price"] = new_price
-        else:
-            print("Item", item_id, "not found. Cannot update price.")
-
-    def sell(self, inventory, item_id: int):
-        if item_id in self.inventory:
-            del self.inventory[item_id]
-            print("Item", item_id, "sold!")
-        else: 
-            print("Item", item_id, "not found. Please select another item to sell.")
-
-    def print_inventory(self, inventory):
-        # If the inventory is not empty
-        if self.inventory:
-            # For each item
-            for item_id in self.inventory:
-                # Print its details
-                print(f'Item ID: {item_id} : {self.inventory[item_id]}')
-        else:
-            print("No inventory to display.")
     
     # How will you set up your constructor?
         # - Multiple functions that work to alter the status of the inventory
